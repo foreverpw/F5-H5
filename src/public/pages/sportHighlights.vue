2f1c92d5cebfe974d745bc5fe817a675fad541b4
foreverpw <pwpwforever@gmail.com>
2019-05-26 19:15:33 +0800

    f

--- src/public/pages/sportHighlights.vue
+++ src/public/pages/sportHighlights.vue
@@ -40,7 +40,7 @@ export default {
   data(){
     return {
       video:null,
-      imgs:Object.values(gameImgs).map(i=>location.href+i)
+      imgs:Object.values(gameImgs)//.map(i=>location.href+i)
     }
   },
   methods:{
