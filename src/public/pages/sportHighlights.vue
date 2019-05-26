b1f34b4d89745ac3b95e2b6c06b14d88cc458f5d
foreverpw <pwpwforever@gmail.com>
2019-05-26 19:23:03 +0800

    origin

--- src/public/pages/sportHighlights.vue
+++ src/public/pages/sportHighlights.vue
@@ -40,12 +40,11 @@ export default {
   data(){
     return {
       video:null,
-      imgs:Object.values(gameImgs).map(i=>location.href+'/'+i)
+      imgs:Object.values(gameImgs).map(i=>location.origin+'/'+i)
     }
   },
   methods:{
     preview(url){
-      alert(url)
       window.jsSDKReady.then(({wx})=>{
         wx.previewImage({
           current: url, // 当前显示图片的http链接
