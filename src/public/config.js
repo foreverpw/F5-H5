cf9be521a51fc3765023a269896c6b68c7d650bc
foreverpw <pwpwforever@gmail.com>
2019-05-26 19:54:44 +0800

    dsf

--- src/public/config.js
+++ src/public/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // serviceDomain:'http://47.102.197.119:12200'
-  serviceDomain:'https://hupu.ksspark.cn:12200'
+  serviceDomain:'http://47.102.197.119:12200'
+  // serviceDomain:'https://hupu.ksspark.cn:12200'
 }
 if(process.env.ENV=='test'){
   // config.serviceDomain = 'http://192.168.3.130:12200'
